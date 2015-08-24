8c55b2498d809f36fa440c0816b2aa476354e4c3
Fabian Canas <fcanas@gmail.com>
2015-08-24 16:18:30 -0400

    Initial commit

---
+++ Foxes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Foxes
+//
+//  Created by Fabian Canas on 8/24/15.
+//  Copyright © 2015 Fabian Canas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
